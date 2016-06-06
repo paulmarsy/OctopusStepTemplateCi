6ab69cc586a0a461a6774f8b97da35346ace2670
Paul Marston <paulmarsy@users.noreply.github.com>
2016-06-06 19:21:06 +0100

    Update Export-CmdletParameters.ps1

--- OctopusStepTemplateCi/Cmdlets/Interface/Export-CmdletParameters.ps1
+++ OctopusStepTemplateCi/Cmdlets/Interface/Export-CmdletParameters.ps1
@@ -1,6 +1,39 @@
+<#
+Copyright 2016 ASOS.com Limited
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+#>
+
+<#
+.NAME
+    Export-CmdletParameters
+    
+.SYNOPSIS
+    Exports the parameters of another cmdlet
+.DESCRIPTION
+    Exports the parameters of another cmdlet into the metadata variable format used by the module,
+    so you can run for example, Export-CmdletParameters -Name Get-Help and it will generate the $StepTemplateParameters array    
+    
+.PARAMETER Name
+    The name of the cmdlet to export the parameters of.
+    
+.INPUTS
+    None. You cannot pipe objects to Export-CmdletParameters.
+.OUTPUTS
+    None.
+#>
 function Export-CmdletParameters {
+    [CmdletBinding()]
+    [OutputType("System.String")]
     param(
-        $Name
+        [Parameter(Mandator=$true)][System.String]$Name
     )
     
     $cmdlettMetaData = Get-Help -Name $Name 
